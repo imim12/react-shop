ecd41bd4c2f1541811fd2c35a54dd1dc8a58b800
imj <imijij25@gamil.com>
2023-08-19 21:24:27 +0900

    상세페이지 데이터 생성하기

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import {useAppDispatch} from '../../hooks/redux';
+import { fetchProduct } from '../../store/products/product.slice';
 
 const DetailPage = () => {
+
+  const {id} = useParams();  //url로 온 파라미터값을 받아서 사용하도록
+  const productId = Number(id);  //String 타입 id를 number로 변환
+  const dispatch = useAppDispatch();
+
+
+  useEffect(() => {
+    
+    dispatch(fetchProduct(productId));
+  
+  }, [productId])
+  
+
   return (
     <div>index</div>
   )
