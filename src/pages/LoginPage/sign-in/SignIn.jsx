564b3ad8b54dbcf282d3c7c40f5a7da334c83de9
imj <imijij25@gamil.com>
2023-08-19 11:38:09 +0900

    Login, Register 페이지 UI 생성하기

---
+++ src/pages/LoginPage/sign-in/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Form } from 'react-router-dom'
+
+const SignIn = () => {
+  return (
+    <Form
+        title={"로그인"}
+    />
+  )
+}
+
+export default SignIn
---
+++ src/pages/RegisterPage/sign-up/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Form } from 'react-router-dom'
+
+const SignUp = () => {
+  return (
+    <Form
+    title={"가입하기"}
+    />
+  )
+}
+
+export default SignUp
