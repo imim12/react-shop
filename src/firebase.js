8d106f7f8c5637067c269369369bc81230ac3b77
imj <imijij25@gamil.com>
2023-08-19 14:05:12 +0900

    파이어베이스 연결하기

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-_kRAfDO32PttwepcYZQMulhEkMC5OtU",
+  authDomain: "react-shop-app-40494.firebaseapp.com",
+  projectId: "react-shop-app-40494",
+  storageBucket: "react-shop-app-40494.appspot.com",
+  messagingSenderId: "760587184367",
+  appId: "1:760587184367:web:f8b8ecbc6d35d6b6c3531a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
