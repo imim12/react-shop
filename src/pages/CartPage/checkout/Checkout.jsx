6fea7ade6eae44bf0a8550e653f5db94a3d60e82
imj <imijij25@gamil.com>
2023-08-20 16:14:46 +0900

    Checkout 컴포넌트 생성하기

--- src/pages/CartPage/checkout/Checkout.jsx
+++ src/pages/CartPage/checkout/Checkout.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
+import { getTotalPrice } from '../../../store/cart/cart.slice';
+import styles from './Checkout.module.scss'
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
 
 const Checkout = () => {
+
+    const cart = useAppSelector(state => state.cartSlice);
+    const dispatch = useAppDispatch()
+    const {isAuth} = useAuth();
+
+    useEffect(()=>{
+        dispatch(getTotalPrice())
+    },[cart])
+
   return (
-    <div>Checkout</div>
+    <div className={styles.checkout}>
+        <div>
+            <p>
+                {" "}
+                <span>합계:</span> ${cart.totalPrice.toFixed(2)}
+            </p>
+            {isAuth ? 
+                <button
+                    className={styles.checkout_button}
+                >
+                    계산하기
+                </button>
+            :
+                <Link className={styles.checkout_button} to='/login'>
+                    로그인
+                </Link>
+            } 
+            
+        </div>
+    </div>
   )
 }
 
