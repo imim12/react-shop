b5c803eb1c49aa9f54073bfac0b65fae8b5f5d09
imj <imijij25@gamil.com>
2023-08-21 09:58:27 +0900

    타입스크립트를 사용하기 위한 준비하기

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './global.scss'
 import { Provider } from 'react-redux'
 import { store } from './store/index.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <App />
   </Provider>,
